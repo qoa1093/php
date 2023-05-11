97ee4f7805ff1f55f72c16ed14c1b258df436299
chacha <qoa1093@gmail.com>
2023-05-11 09:37:11 +0900

    php: 5월 10일

---
+++ quote1.php
@@ -0,0 +1,15 @@
+<?php
+$name = "홍지수";
+//echo $name;
+echo "$name 님 반갑습니다.";
+echo "<br>";
+
+$name = '홍지수';
+//echo $name;
+echo '$name 님 반갑습니다.';
+echo '<br>';
+
+echo "{$name}님 반갑습니다.";
+echo '<br>';
+
+?>
